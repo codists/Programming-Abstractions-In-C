0ca96b957f11762c211af2b6169b66b5e2688cb1
编程人 <codists@foxmail.com>
2023-07-27 22:08:25 +0800

    Update arraybase.c
    add initializer demo

--- chapter2/arraybase.c
+++ chapter2/arraybase.c
@@ -1,8 +1,15 @@
+/*
+ *
+ */
 #include <stdio.h>
 #define  NJudges 5
 
 int main(int argc, char const *argv[])
 {
+    // 概念：initializer
+    int digits[5] = {1, 2, 3, 4, 5}; // {1, 2, 3, 4, 5}称为initializer
+    printf("test");
+
     // Array declaration p69: type name[size];
     double scores[NJudges]; // 为了方便后续修改，所以数组的大小NElements声明为symbolic constant
 
@@ -12,6 +19,17 @@ int main(int argc, char const *argv[])
     scores[0] = 9.7;
     scores[0] = 9.0;
     scores[0] = 9.5;
+
+    static string bigCities[] = {
+        "New York",
+        "Los Angeles",
+        "Chicago",
+        "Houston",
+        "Philadelphia",
+        "San Diego",
+        "Detroit",
+        "Dallas"
+    };
 
     return 0;
 }
