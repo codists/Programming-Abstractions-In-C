eaa93100b6f262ba9abf6b23b58d5aa79852f918
codists <codists@foxmail.com>
2023-07-31 07:24:30 +0800

    update character array

--- chapter2/longestline.c
+++ chapter2/longestline.c
@@ -23,17 +23,17 @@ int main() {
     return 0;
 }
 
-/* getline: read a line into s, return length */
+/* 读取输入的字符然后存入到数组中，并返回该数组的长度*/
 int getline(char s[], int lim) {
-    int c, i;
+    int c, i; // c: 输入的字符。i: 行数。
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
         s[i] = c;
     if (c == '\n') {
-        s[i] = c;
-        ++i;
+        s[i] = c; // 换行符也需要加入到存入数组
+        ++i; // 如果输入的是换行符，那么行数加1
     }
-    s[i] = '\0';
-    return i;
+    s[i] = '\0'; // 输入结束，最后一个字符为0
+    return i; // 返回行的长度
 }
 
 /* copy: copy 'from' into 'to'; assume to is big enough */
